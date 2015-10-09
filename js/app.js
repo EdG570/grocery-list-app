ac69da31a032f815153ac18940b22359f08e85d8
Ed Glazenski <edglazenski@gmail.com>
2015-10-09 15:12:20 -0400

    Fixed input field not clearing on submit

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     event.preventDefault();
        var itemAdded = $('input[name="add"]').val();
       $('.shopping-list').append('<li><button class="complete-btn"><img class="check-mark" src="images/check.svg"/></button><div class="list-item"><p>' + itemAdded + '</p></div><button class="remove-btn">X</button></li>');
+      $('form').children('.add-item').val('');
   });
 
 //Allows users to check and uncheck complete button in their list
